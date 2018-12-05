3f7c9e4c1a91ddddb9761119fc1e9c4384476e31
Raheel <moh13357822@students.solihull.ac.uk>
2018-12-05 11:58:12 +0000

    Health bar tutorial

---
+++ Health bar/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour {
+
+    [SerializeField] 
+
+
+    private float fillAmount; 
+
+
+
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        Handlebar(); 
+
+    }
+
+    private void Handlebar()
+    {
+
+
+
+
+    } 
+
+
+
+
+}
