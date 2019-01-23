86da4d77f3b7857f2d39aa8c36bae6058a8f9f27
Raheel21 <moh13357822@students.solihull.ac.uk>
2019-01-23 00:36:35 +0000

    new scripts

--- Health bar/Assets/Scripts/Healthbar.cs
+++ Health bar/Assets/Scripts/Healthbar.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Healthbar : MonoBehaviour {
 
-    [SerializeField] 
+    private float fillAmount;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    [SerializeField]
+    private Image content;
+
+    [SerializeField]
+    private Text Valuetext; 
 
 
-    private float fillAmount; 
 
+    public float MaxValue { get; set; }
 
+    public float Value
 
+    {
+        set
+        {
 
-
-
+            string[] tmp = Valuetext.text.Split(':');
+            Valuetext.text = tmp[0] + ":" + value; 
+            fillAmount = Map(value, 0, MaxValue, 0, 1); 
+        }
+    }
     // Use this for initialization
     void Start() {
 
@@ -23,20 +40,19 @@ public class Healthbar : MonoBehaviour {
     void Update()
 
     {
-
         Handlebar(); 
-
     }
 
     private void Handlebar()
     {
-
-
-
-
+        if (fillAmount != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, fillAmount, Time.deltaTime * lerpSpeed); 
+        }
     } 
 
-
-
-
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin; 
+    }
 }
---
+++ Health bar/Assets/Scripts/Stat.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Stat
+{
+    [SerializeField]
+    private Healthbar healthbar;
+
+    [SerializeField]
+    private float maxVal;
+
+    [SerializeField]
+    private float currentVal;
+
+    public float CurrentVal
+    {
+        get
+        {
+            return currentVal;
+        }
+
+        set
+        {
+            
+
+            currentVal = Mathf.Clamp(value, 0, MaxVal);
+            healthbar.Value = currentVal;
+        }
+    }
+
+    public float MaxVal
+    {
+        get
+        {
+            return maxVal;
+        }
+
+        set
+        {
+            maxVal = value;
+            healthbar.MaxValue = maxVal;
+        }
+    }
+
+    public void Initialize()
+    {
+        MaxVal = maxVal;
+        CurrentVal = currentVal; 
+    }
+}
