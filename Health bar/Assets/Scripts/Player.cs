47fed993e26a11607b28f04e71a89a25a8b2027d
Raheel <moh13357822@students.solihull.ac.uk>
2019-03-06 11:45:55 +0000

    added script
    character now loses health when colliders with enemy

--- Health bar/Assets/Scripts/Player.cs
+++ Health bar/Assets/Scripts/Player.cs
@@ -2,41 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     [SerializeField]
     private Stat health;
 
-    
+
     private void Awake()
     {
+
         health.Initialize();
 
-              
     }
 
-    
-    // Update is called once per frame
-   void update()
+
+    void OnTriggerEnter2D (Collider2D other)
     {
+        if (other.gameObject.tag == "Enemy")
+        {
 
+            health.CurrentVal -= 10;
+            Debug.Log("hits");
 
+        }
     }
-
-
-
-
-
-            
-        //(Input.GetKeyDown(KeyCode.S))
-        //{
-            //health.CurrentVal -= 10;
-        //}
-
-     //if (Input.GetKeyDown(KeyCode.W))
-       // {
-
-         //   health.CurrentVal += 10; 
-        //}
-
-    }
+}
